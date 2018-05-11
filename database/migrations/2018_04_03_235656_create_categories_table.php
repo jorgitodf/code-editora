93f69ba0e0138a5090378ee45325db219f949aa9
jorgitodf <jspaiva.1977@gmail.com>
2018-05-11 14:26:46 -0300

    Fase 7: Envio de E-mails - correção

--- database/migrations/2018_04_03_235656_create_categories_table.php
+++ database/migrations/2018_04_03_235656_create_categories_table.php
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
             $table->timestamps();
@@ -27,6 +27,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
